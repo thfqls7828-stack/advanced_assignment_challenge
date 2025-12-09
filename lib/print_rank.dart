d7e16aa9fa7bce1d37d59377b4ef8e94e8a318f4
glory <thfqls7828@gmail.com>
2025-12-09 18:56:11 +0900

    style : 주석 변경

--- lib/print_rank.dart
+++ lib/print_rank.dart
@@ -2,6 +2,8 @@ import 'package:advanced_assignment_challenge/score_student.dart';
 
 void printRank(List<ScoreStudent> studentList) {
   ScoreStudent bestStudent;
+
+  // 이런 방법도 있다.
   // int temp = 0;
 
   // for (ScoreStudent student in studentList) {
